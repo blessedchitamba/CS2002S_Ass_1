4cb2f2516535c3ac4417e501a278151da32a1664
blessedchitamba <blessedbrighton@live.com>
2018-08-11 15:23:35 +0200

    where i am now. trying the Save button functionality

--- app/src/main/java/com/watertracker/watertracker/Calculator_activity.java
+++ app/src/main/java/com/watertracker/watertracker/Calculator_activity.java
@@ -2,12 +2,92 @@ package com.watertracker.watertracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Calculator_activity extends AppCompatActivity {
+
+    private String[] entry_data = {"date", "shower", "toilet", "hygiene", "laundry", "dishes", "drinking",
+                                    "cooking", "cleaning", "other"};
+    private String[] strings = new String[10];
+    private static final String FILE_NAME = "example.txt";
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator_activity);
+    }
+
+    public void save(View view) {
+        //load the input strings into the array. clear the text in each input field at each count
+        editText = findViewById(R.id.date);
+        strings[0] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.input1);
+        strings[1] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.input2);
+        strings[2] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.input3);
+        strings[3] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.laundry);
+        strings[4] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.dishes);
+        strings[5] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.drinking);
+        strings[6] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.cooking);
+        strings[7] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.cleaning);
+        strings[8] = editText.getText().toString();
+        editText.getText().clear();
+
+        editText = findViewById(R.id.other);
+        strings[9] = editText.getText().toString();
+        editText.getText().clear();
+
+        //start writing the strings into file. iterate through strings array
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            for (String s: strings) {
+                fos.write(s.getBytes());
+            }
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+    } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
